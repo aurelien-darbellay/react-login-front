6d8e08399b4c70cbcf2a18c28a41900af89b1dec
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-03-12 18:29:10 +0100

    improved look, corrected publicUser generation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
       <div className="p-6 bg-white shadow-lg rounded-lg w-96">
         <h2 className="text-2xl font-semibold text-center">Login</h2>
         {error && <p className="text-red-500 text-sm text-center">{error}</p>}
-        <form onSubmit={handleLogin} className="flex flex-col gap-4 mt-4">
+        <form onSubmit={handleLogin} className="flex flex-row gap-4 mt-4 mb-4">
           <input
             type="text"
             placeholder="Username"
@@ -55,7 +55,7 @@ const Login = () => {
           />
           <button
             type="submit"
-            className="bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600"
+            className="bg-blue-500 p-2 rounded-lg hover:bg-blue-600"
           >
             Login
           </button>
